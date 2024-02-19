96e8dbbbe4058925a51793b57ec219e68df6a5e5
Debroop Ghosh <debroopghosh2012@gmail.com>
2024-02-19 14:04:58 +0530

    added the final project code

--- Bg_color_changing/src/App.jsx
+++ Bg_color_changing/src/App.jsx
@@ -1,15 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("Grey")
 
   return (
-    <>
-      
-    </>
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor: color}}
+    >
+    <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+      <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white padding px-3 py-2 rounded-3xl"  >
+          <button onClick={() => setColor("red")}   className="outline-none px-4 py-1 rounded-full text-white" style={{backgroundColor: "red"}} >
+            Red
+          </button>
+          <button onClick={() => setColor("Blue")} className="outline-none px-4 py-1 rounded-full text-white" style={{backgroundColor: "Blue"}} >
+            Blue
+          </button>
+          <button onClick={() => setColor("Green")}  className="outline-none px-4 py-1 rounded-full text-white" style={{backgroundColor: "Green"}} >
+            Green
+          </button>
+          <button onClick={() => setColor("Black")}  className="outline-none px-4 py-1 rounded-full text-white" style={{backgroundColor: "Black"}} >
+            Black
+          </button>
+        
+         </div>
+      </div>
+    </div>
   )
 }
 
